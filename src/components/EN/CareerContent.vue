e33e1e28cd887b4b0270dc1de8254bab723ccf96
yds04089 <yds04089@naver.com>
2021-08-02 18:22:51 +0900

    210802 Career page

---
+++ src/components/EN/CareerContent.vue
@@ -0,0 +1,529 @@
+<template>
+  <div class="career-content-wrapper">
+    <!-- STRATEGIC ACCOUNT EXECUTIVE -->
+    <div class="career-content" v-if="props.careerId === 0">
+      <div class="career-content-title">
+        <span>STRATEGIC ACCOUNT EXECUTIVE</span>
+        <!-- <button @click=";[(nowId = -1), onSelect(nowId)]">지원하기</button> -->
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">Key Responsibilities</div>
+        <ul class="content-item-content">
+          <li>
+            Create and implement an outbound sales strategy for your initial
+            region.
+          </li>
+          <li>
+            Create a vision, mission, ICPs, and target market for Zenerate.
+          </li>
+          <li>
+            Collaborate with the business development team to generate inbound
+            leads.
+          </li>
+          <li>
+            Build a strong pipeline and land at least 25 new accounts per
+            period.
+          </li>
+          <li>Lead the expansion of Zenerate into other cities.</li>
+          <li>Deliver your initial round of new sales.</li>
+          <li>
+            Deliver three times the number of new sales in the first three
+            months.
+          </li>
+          <li>Collaborate with internal teams to execute rapid growth plan.</li>
+          <li>
+            Share important customer insights with US and international teams.
+          </li>
+        </ul>
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">Requirements</div>
+        <ul class="content-item-content">
+          <li>
+            Entrepreneurial team player, quick to adapt to changing situations
+          </li>
+          <li>
+            Proven track record of lead & pipeline generation, meeting and
+            surpassing sales quotas
+          </li>
+          <li>
+            Minimum 5 years of SaaS / Software sales experience from
+            multinational companies
+          </li>
+          <li>
+            Strong listener with excellent presentation and closure skills
+          </li>
+          <li>Account planning and execution skills</li>
+          <li>Eloquent & effective written, verbal, interpersonal skills</li>
+          <li>
+            Ability to build and mainatain professional relationships with
+            C-level executives
+          </li>
+          <li>
+            A passion for human-centered design and creative problem solving
+          </li>
+          <li>
+            Must be highly self-motivated and able to work independently,
+            proactively
+          </li>
+          <li>
+            Be willing to wear multiple hats as the company grows and expands
+          </li>
+        </ul>
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">Ideal but not necessary</div>
+        <ul class="content-item-content">
+          <li>
+            Experience in related industries: PropTech, real estate development,
+            urban planning, construction, architecture
+          </li>
+        </ul>
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">Company benefits</div>
+        <ul class="content-item-content">
+          <li>Flexible work hours after the first 90-day period</li>
+          <li>Professional development learning stipend</li>
+          <li>Daily lunch stipend</li>
+          <li>Cell phone reimbursement</li>
+          <li>Travel reimbursement</li>
+          <li>Health and wellness perks</li>
+        </ul>
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">How to apply</div>
+        <ul class="content-item-content">
+          <li>Send resume and cover to sjoo@zenerate.ai</li>
+        </ul>
+      </div>
+
+      <button
+        class="back-button"
+        @click=";[(nowId = -1), onSelect(nowId), scrollTop()]"
+      >
+        <span>BACK</span>
+        <svg
+          width="122"
+          height="28"
+          viewBox="0 0 122 28"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M121 27H0.999997L27.6886 1"
+            stroke="#4747FF"
+            stroke-linecap="square"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </button>
+    </div>
+    <!-- BUSINESS DEVELOPMENT MANAGER -->
+    <div class="career-content" v-if="props.careerId === 1">
+      <div class="career-content-title">
+        <span>BUSINESS DEVELOPMENT MANAGER</span>
+        <!-- <button @click=";[(nowId = -1), onSelect(nowId)]">지원하기</button> -->
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">Key Responsibilities</div>
+        <ul class="content-item-content">
+          <li>
+            Represent Zenerate with professional courtesy and enthusiasm to all
+            customers and stakeholders
+          </li>
+          <li>
+            Own the entire sales process for your dedicated book of business,
+            from developing sales strategy to building the pipeline of deals.
+          </li>
+          <li>Channel the customer voice to the Product team (Korea).</li>
+          <li>Sign your first paid accounts with Zenerate customers</li>
+          <li>
+            Establish a strong pipeline of prospects with projected revenues and
+            timelines.
+          </li>
+          <li>
+            Attend industry events, partnered with customer success to improve
+            our onboarding and support model, and provided customer feedback to
+            influence the product roadmap.
+          </li>
+          <li>
+            Provide feedback and support to improve our sales process and
+            strategy, to scale Zenerate to a billion-dollar business.
+          </li>
+          <li>
+            Work with account executive and team to launch additional regions
+            and expand our presence in the industry
+          </li>
+          <li>
+            Maintain sales support materials including sales presentations,
+            portfolio images and case studies..
+          </li>
+          <li>
+            Write and review proposals for accurate content and consistent
+            language and formatting to ensure the highest quality of proposal is
+            submitted to the client.
+          </li>
+        </ul>
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">Requirements</div>
+        <ul class="content-item-content">
+          <li>
+            Entrepreneurial team player, quick to adapt to changing situations
+          </li>
+          <li>
+            Proven track record of lead & pipeline generation, meeting and
+            surpassing sales quotas
+          </li>
+          <li>
+            Minimum 5 years of SaaS / Software sales experience from
+            multinational companies
+          </li>
+          <li>
+            Strong listener with excellent presentation and closure skills
+          </li>
+          <li>Account planning and execution skills</li>
+          <li>Eloquent & effective written, verbal, interpersonal skills</li>
+          <li>
+            Ability to build and maintain professional relationships with
+            C-level executives
+          </li>
+          <li>
+            A passion for human-centered design and creative problem solving
+          </li>
+          <li>
+            Must be highly self-motivated and able to work independently,
+            proactively
+          </li>
+          <li>
+            Be willing to wear multiple hats as the company grows and expands
+          </li>
+        </ul>
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">Ideal but not necessary</div>
+        <ul class="content-item-content">
+          <li>
+            Experience in related industries: PropTech, real estate development,
+            urban planning, construction, architecture
+          </li>
+        </ul>
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">Company benefits</div>
+        <ul class="content-item-content">
+          <li>Flexible work hours after the first 90-day period</li>
+          <li>Professional development learning stipend</li>
+          <li>Daily lunch stipend</li>
+          <li>Cell phone reimbursement</li>
+          <li>Travel reimbursement</li>
+          <li>Health and wellness perks</li>
+        </ul>
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">How to apply</div>
+        <ul class="content-item-content">
+          <li>Send resume and cover to sjoo@zenerate.ai</li>
+        </ul>
+      </div>
+
+      <button class="back-button" @click=";[(nowId = -1), onSelect(nowId)]">
+        <span>BACK</span>
+        <svg
+          width="122"
+          height="28"
+          viewBox="0 0 122 28"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M121 27H0.999997L27.6886 1"
+            stroke="#4747FF"
+            stroke-linecap="square"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </button>
+    </div>
+    <!-- BUSINESS DEVELOPMENT & MARKETING INTERN -->
+    <div class="career-content" v-if="props.careerId === 2">
+      <div class="career-content-title">
+        <span>BUSINESS DEVELOPMENT & MARKETING INTERN</span>
+        <!-- <button @click=";[(nowId = -1), onSelect(nowId)]">지원하기</button> -->
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">Key Responsibilities</div>
+        <ul class="content-item-content">
+          <li>
+            Assist the business development team in maintaining up-to-date
+            client contacts and company records in the CRM system.
+          </li>
+          <li>
+            Manage CRM related changes to project status, design authorization,
+            and purchase orders. Follow-up with project managers to update them
+            on completed changes.
+          </li>
+          <li>
+            Assist in the writing and review proposals for accurate content and
+            consistent language and formatting to ensure the highest quality of
+            proposal is submitted to the client.
+          </li>
+          <li>
+            Assist the business development team in entering CRM information for
+            prospective client targets.
+          </li>
+          <li>
+            Follow-up on past due invoices and support business development and
+            accounting in dispute resolution.
+          </li>
+          <li>
+            Provide general sales support as needed. Tasks in this area can
+            include: CRM training and maintenance, helping with document
+            problems and mailings.
+          </li>
+          <li>Create/maintain sales reports.</li>
+          <li>
+            Maintain sales support materials including sales presentations,
+            portfolio images and case studies.
+          </li>
+          <li>
+            Maintain sales related document library including purchase orders,
+            legal contracts, confidentiality agreements.
+          </li>
+        </ul>
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">Requirements</div>
+        <ul class="content-item-content">
+          <li>3 years previous administrative experience</li>
+          <li>Minimum 2 years experience using a CRM system</li>
+          <li>Proficiency in Google Suite, Microsoft Office</li>
+          <li>
+            Entrepreneurial team player, quick to adapt to changing situations
+          </li>
+          <li>
+            Proven track record of lead & pipeline generation, meeting and
+            surpassing sales quotas
+          </li>
+          <li>Eloquent & effective written, verbal, interpersonal skills</li>
+          <li>
+            A passion for human-centered design and creative problem solving
+          </li>
+          <li>
+            Must be highly self-motivated and able to work independently,
+            proactively
+          </li>
+          <li>
+            Able to travel with business development team, whenever necessary
+          </li>
+        </ul>
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">Ideal but not necessary</div>
+        <ul class="content-item-content">
+          <li>
+            Experience in related industries: PropTech, real estate development,
+            urban planning, construction, architecture
+          </li>
+          <li>SaaS / Software sales experience from multinational companies</li>
+        </ul>
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">Company benefits</div>
+        <ul class="content-item-content">
+          <li>Flexible work hours after the first 90-day period</li>
+          <li>Professional development learning stipend</li>
+          <li>Daily lunch stipend</li>
+          <li>Health and wellness perks</li>
+        </ul>
+      </div>
+      <div class="content-item">
+        <div class="content-item-title">How to apply</div>
+        <ul class="content-item-content">
+          <li>Send resume and cover to sjoo@zenerate.ai</li>
+        </ul>
+      </div>
+      <button class="back-button hover-pointer" @click=";[(nowId = -1), onSelect(nowId)]">
+        <a href="#firstPage"> <span>BACK</span></a>
+        <svg
+          width="122"
+          height="28"
+          viewBox="0 0 122 28"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M121 27H0.999997L27.6886 1"
+            stroke="#4747FF"
+            stroke-linecap="square"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineEmit, defineProps, onMounted, computed } from 'vue'
+import Store from '/Store/index'
+const fp = computed(() => Store.state.root.FullPage)
+
+onMounted(() => {
+  fp.value.init('#fullpage', {
+    scrollOverflow: true,
+    anchors: ['firstPage', 'secondPage', 'thirdPage'],
+  })
+})
+const emit = defineEmit(['onSelect'])
+const onSelect = (value) => {
+  console.log(value)
+  emit('onSelect', value)
+}
+const props = defineProps({
+  careerId: {
+    type: Number,
+  },
+})
+const scrollTop = () => {
+  window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+}
+const goToApply = () => {
+  window.open(
+    'https://docs.google.com/forms/d/1poAA0R2ZpVIgo22jaRwR3RmjIXZwKiZAoc7lusY2Gc4/viewform?edit_requested=true',
+    '_blank'
+  )
+}
+</script>
+<style lang="scss" scoped>
+.career-content-wrapper {
+  height: 100%;
+  width: 100%;
+  margin: auto;
+  padding-bottom: 100px;
+  .career-content {
+    // .image-wrapper {
+    //   width: 100%;
+    //   margin-bottom: 40px;
+    //   @include tablet {
+    //     margin-bottom: 30px;
+    //   }
+    //   @include mobile {
+    //     margin-bottom: 20px;
+    //   }
+    //   img {
+    //     width: 100%;
+    //     height: 100%;
+    //     object-fit: contain;
+    //   }
+    // }
+    .career-content-title {
+      @include flex($justify: space-between);
+      padding-bottom: 40px;
+      border-bottom: solid 1px $grey-3;
+      margin-bottom: 40px;
+      @include tablet {
+        padding-bottom: 30px;
+      }
+      @include mobile {
+        padding-bottom: 30px;
+      }
+      span {
+        @include bold(40);
+        line-height: 60px;
+        @include tablet {
+          @include bold(32);
+        }
+        @include mobile {
+          @include bold(28);
+        }
+      }
+      button {
+        margin: auto 0;
+        color: white;
+        width: 144px;
+        height: 48px;
+        border-radius: 100px;
+        background-color: $main;
+        @include tablet {
+          width: 100px;
+          height: 40px;
+          @include medium(14);
+        }
+        @include mobile {
+          width: 80px;
+          height: 40px;
+          @include medium(12);
+        }
+      }
+    }
+    .content-item {
+      .content-item-title {
+        @include bold(20);
+        margin-bottom: 10px;
+        @include tablet {
+          @include bold(16);
+        }
+        @include mobile {
+          @include bold(14);
+        }
+      }
+      .content-item-content {
+        list-style-type: disc;
+        margin-left: 28px;
+        margin-bottom: 40px;
+        @include medium(16);
+        color: rgba($black-1, 0.7);
+        .caption-large {
+          @include bold(16);
+          line-height: 40px;
+          @include tablet {
+            @include bold(14);
+          }
+          @include mobile {
+            @include bold(12);
+          }
+        }
+        @include tablet {
+          @include medium(14);
+        }
+        @include mobile {
+          @include medium(12);
+        }
+      }
+      .content-item-content-nolist {
+        margin-left: 0px;
+        @include tablet {
+          p {
+            margin-bottom: 12px;
+          }
+        }
+        @include mobile {
+          p {
+            margin-bottom: 12px;
+          }
+        }
+      }
+    }
+    .back-button {
+      @include center-center;
+      margin: 48px auto 6px auto;
+      @include bold(16);
+      span {
+        padding-bottom: 5px;
+      }
+      color: $main;
+      svg {
+        position: absolute;
+      }
+      @include tablet {
+        @include bold(14);
+      }
+      @include mobile {
+        @include bold(14);
+      }
+    }
+  }
+}
+</style>
