4d11221b5eaa3a8ea5af9aa804a995f8ffd79b12
coldsewoo <coldsewoo@gmail.com>
2021-08-04 13:03:54 +0900

    210804/landing page작업(1)

---
+++ src/constants/partners.ts
@@ -0,0 +1,42 @@
+export default [
+  {
+    key: 'planh',
+    description: 'Corporate VC of Hoban',
+  },
+  {
+    key: 'bluepoint',
+    description: 'Korea’s #1 accelerator',
+  },
+  {
+    key: 'dsnetworks',
+    description: 'Top real estate developer in Korea',
+  },
+  {
+    key: 'deloitte',
+    description: 'Top real estate consulting firm in Korea',
+  },
+  {
+    key: 'oneoone',
+    description: 'VC',
+  },
+  {
+    key: 'hoban',
+    description: 'Top-tier construction company',
+  },
+  {
+    key: 'krypton',
+    description: 'vc',
+  },
+  {
+    key: 'shinyoung',
+    description: 'Leading real estate developer in Korea',
+  },
+  {
+    key: 'mdm',
+    description: 'VC',
+  },
+  {
+    key: 'neovalue',
+    description: 'VC',
+  },
+]
--- src/views/EN/LandingView.vue
+++ src/views/EN/LandingView.vue
@@ -27,7 +27,9 @@
         </div>
       </div>
       <div class="landing-background"></div>
-      <div class="landing-background-wave"></div>
+      <div class="landing-background-wave">
+        <img src="/en/img/about_mission_back.png" alt="" />
+      </div>
     </div>
     <div class="section section-engine">
       <div class="section-inner inner-engine">
@@ -52,7 +54,116 @@
       </div>
     </div>
     <div class="section section-instant-search">
-      
+      <div class="section-inner inner-instant-search">instant search</div>
+    </div>
+    <div class="section section-market-insight">
+      <div class="section-inner inner-market-insight">market insight</div>
+    </div>
+    <div class="section section-generation">
+      <div class="section-inner inner-generation">
+        <div class="generation-contents">
+          <div class="content-left">
+            <div class="left-img">
+              <img src="/en/img/landing_generation_mode.png" alt="" />
+            </div>
+            <div class="arrow-bottom">
+              <Arrow></Arrow>
+            </div>
+          </div>
+          <div class="content-right">
+            <div class="right-text">
+              <div class="text-heading">
+                <p>
+                  Need to boost your project profitability and in a time crunch?
+                </p>
+              </div>
+              <div class="text-content">
+                <p>
+                  Let our AI tech <strong>Zenerate™</strong> do the work for you
+                </p>
+              </div>
+              <div class="text-content">
+                <p>
+                  Our customers have seen revenues increase
+                  <strong>up to 12% on average</strong>
+                  compared to manual calculations.
+                </p>
+              </div>
+            </div>
+            <div class="right-img">
+              <img src="/en/img/landing_projects_in_map.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section section-compare">
+      <div class="section-inner inner-compare">
+        <div class="compare-contents">
+          <div class="compare-image">
+            <img src="/en/img/landing_compare.png" alt="" />
+          </div>
+          <div class="compare-text">
+            <div class="text-heading">
+              <p>Compare</p>
+              <p>& pick your best solution</p>
+            </div>
+            <div class="text-content">
+              <p>
+                Or go with the most optimized, profitable solution generated
+                byour powerful AI engine
+              </p>
+            </div>
+            <div class="arrow-bottom">
+              <Arrow></Arrow>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="compare-background"></div>
+    </div>
+    <div class="section section-addtional">
+      <div class="section-inner inner-additional">
+        <div class="additional-text">
+          <p>We’re excited to help you</p>
+          <p>Zenerate™ all possibilities to maximize your success.</p>
+        </div>
+        <div class="additional-line"></div>
+        <div class="additional-blank"></div>
+        <div class="arrow-bottom">
+          <Arrow></Arrow>
+        </div>
+      </div>
+      <div class="additional-wave">
+        <img src="/en/img/about_mission_back.png" alt="" />
+      </div>
+    </div>
+    <div class="section section-partners">
+      <div class="section-inner inner-partners">
+        <div class="partners-text">
+          <p>
+            Join developers, architects, property owners, investors, and
+            planners who trust Zenerate with their projects.
+          </p>
+        </div>
+        <div class="partners-list">
+          <div
+            class="partners-item"
+            v-for="(item, idx) in PartnersList"
+            :key="idx"
+          >
+            <div
+              class="item-logo"
+              :style="{
+                'background-image': `url('/logo/logo_${item.key}.png')`,
+              }"
+            ></div>
+            <div class="item-description">
+              {{ item.description }}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="section section-footer fp-auto-height">
       <Footer />
@@ -65,6 +176,9 @@ import { ref, onMounted, onBeforeUnmount, computed } from 'vue'
 import Footer from '/Components/EN/Footer.vue'
 // @ts-ignore
 import Arrow from '/Components/EN/Arrow.vue'
+
+//misc
+import PartnersList from '/Constants/partners'
 
 import Store from '/Store/index'
 const fullpage = computed(() => Store.state.root.FullPage)
@@ -181,6 +295,165 @@ onBeforeUnmount(() => {
     margin-bottom: 120px;
     justify-content: flex-end;
     padding-right: 20px;
+  }
+}
+
+.section-generation {
+  .generation-contents {
+    width: 100%;
+    height: 100%;
+    @include flex($justify: space-between);
+    .content-left {
+      width: 536px;
+      margin-right: 128px;
+      height: 100%;
+      @include vertical-center;
+      .arrow-bottom {
+        justify-content: flex-start;
+        padding-left: 100px;
+      }
+    }
+    .content-right {
+      @include vertical-center;
+      height: calc(100% - 300px);
+      flex: 1;
+      text-align: left;
+      .right-text {
+        max-width: 500px;
+
+        .text-heading {
+          margin-bottom: 36px;
+          p {
+            @include bold(24);
+          }
+        }
+        .text-content {
+          p {
+            @include regular(18);
+          }
+          margin-bottom: 24px;
+        }
+      }
+
+      .right-img {
+        @include absolute(right 0 bottom 0);
+      }
+    }
+  }
+}
+
+.section-compare {
+  .compare-contents {
+    flex: 1;
+    @include flex($justify: space-between);
+    .compare-image {
+      flex: 1;
+      @include vertical-center;
+      img {
+        max-width: 760px;
+      }
+    }
+    .compare-text {
+      width: 400px;
+      @include vertical-center;
+      text-align: right;
+      .text-heading {
+        @include semi-bold(24);
+        margin-bottom: 48px;
+      }
+      .text-content {
+        p {
+          @include regular(20);
+        }
+      }
+      .arrow-bottom {
+        margin-top: 96px;
+        margin-bottom: 0px;
+        justify-content: flex-end;
+      }
+    }
+  }
+
+  .compare-background {
+    @include absolute(left 0 top 0);
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    background-image: url('/en/img/landing_map.jpg');
+    background-size: cover;
+    background-position: center;
+    opacity: 0.08;
+  }
+}
+
+.section-addtional {
+  .additional-text {
+    margin-top: 80px;
+    margin-bottom: 60px;
+    text-align: center;
+    p {
+      @include semi-bold(24);
+    }
+  }
+  .additional-line {
+    height: 1px;
+    width: 312px;
+    margin: 0px auto;
+    background-color: $grey-2;
+  }
+  .additional-blank {
+    flex: 1;
+  }
+  .arrow-bottom {
+    justify-content: flex-start;
+  }
+  .additional-wave {
+    @include absolute(left 0 top 50%);
+    width: 100%;
+    transform: translateY(-50%);
+    img {
+      width: 100%;
+    }
+  }
+}
+
+.section-partners {
+  .section-inner {
+    justify-content: center;
+  }
+  .partners-text {
+    width: 700px;
+    margin: 0px auto;
+    text-align: center;
+    p {
+      @include regular(18);
+    }
+    margin-bottom: 96px;
+  }
+  .partners-list {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-auto-flow: dense;
+    grid-column-gap: 24px;
+    grid-row-gap: 24px;
+    .partners-item {
+      .item-logo {
+        width: calc(100% - 20px);
+        margin: 0px auto;
+        height: 76px;
+        margin-bottom: 8px;
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+      }
+      .item-description {
+        width: 100%;
+        text-align: center;
+        p {
+          @include regular(13);
+        }
+      }
+    }
   }
 }
 </style>
