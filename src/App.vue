f67a395eec03c23789a42380e380a3b237c5b74f
p2cn2c <snam.zenerate.ai@gmail.com>
2023-05-24 17:29:25 +0900

    Improve: update seo2

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,4 @@
 <template>
-  <metainfo>
-    <template v-slot:title="{ metainfo }">
-      {{ metainfo.title ? `${metainfo.title}` : `Zenerate` }}
-    </template>
-
-    <template v-slot:description="{ metainfo }">{{
-      metainfo.description ? metainfo.description : ''
-    }}</template>
-  </metainfo>
   <router-view></router-view>
 </template>
 
@@ -15,75 +6,67 @@
 import { watch, onMounted, reactive, computed } from 'vue'
 import AOS from 'aos'
 import { useHead } from '@vueuse/head'
-const siteData = reactive({
-  title: 'Zenerate: Creating AI-Powered Tools for Real Estate Development',
-  link: [{ rel: 'canonical', href: 'https://www.zenerate.ai' }],
-  description: `Zenerate provides AI-powered feasibility solutions for real estate development. Explore all possibilities, maximize returns. Zenerate™ App.`,
-  og: {
-    url: 'https://www.zenerate.ai/',
-    type: 'website',
-    title: 'Zenerate: Creating AI-Powered Tools for Real Estate Development',
-    description: `Zenerate provides AI-powered feasibility solutions for real estate development. Explore all possibilities, maximize returns. Zenerate™ App.`,
-    tags: 'zenerate',
-    keywords: 'zenerate',
-    image: 'https://www.zenerate.ai/img/logo_og.png',
-  },
-  twitter: {
-    title: 'Zenerate: Creating AI-Powered Tools for Real Estate Development',
-  },
-})
 
 useHead({
-  title: computed(() => siteData.title),
+  title: 'Zenerate: Creating AI-Powered Tools for Real Estate Development',
   htmlAttrs: { lang: 'en' },
-  link: computed(() => siteData.link),
+  link: [{ rel: 'canonical', href: 'https://www.zenerate.ai' }],
   meta: [
     {
       name: `description`,
-      content: computed(() => siteData.description),
-    },
-    {
-      name: 'twitter:card',
-      content: 'summary_large_image',
-    },
-    {
-      name: 'twitter:title',
-      content: computed(
-        () => 'Zenerate: Creating AI-Powered Tools for Real Estate Development'
-      ),
-    },
-    {
-      name: 'twitter:description',
       content: `Zenerate provides AI-powered feasibility solutions for real estate development. Explore all possibilities, maximize returns. Zenerate™ App.`,
     },
-    { name: 'keywords', content: 'zenerate' },
     {
-      name: 'twitter:image',
-      content: 'https://www.zenerate.ai/img/logo_og.png',
-    },
-    {
-      name: 'og:url',
-      content: 'https://www.zenerate.ai/',
-    },
-    {
-      name: 'og:title',
+      hid: 'twitter:title',
+      property: 'twitter:title',
       content:
         'Zenerate: Creating AI-Powered Tools for Real Estate Development',
     },
     {
-      name: 'og:description',
-      content: `Zenerate provides AI-powered feasibility solutions for real estate development. Explore all possibilities, maximize returns. Zenerate™ App`,
+      hid: 'twitter:description',
+      property: 'twitter:description',
+      content: `Zenerate provides AI-powered feasibility solutions for real estate development. Explore all possibilities, maximize returns. Zenerate™ App.`,
     },
+    { name: 'keywords', content: 'zenerate' },
     {
-      name: 'og:type',
-      content: 'website',
-    },
-    {
-      name: 'og:image:url',
+      hid: 'twitter:image',
+      property: 'twitter:image',
       content: 'https://www.zenerate.ai/img/logo_og.png',
     },
     {
-      name: 'og:image:type',
+      hid: 'twitter:card',
+      property: 'twitter:card',
+      content: 'summary_large_image',
+    },
+    {
+      hid: 'og:url',
+      property: 'og:url',
+      content: 'https://www.zenerate.ai/',
+    },
+    {
+      hid: 'og:title',
+      property: 'og:title',
+      content:
+        'Zenerate: Creating AI-Powered Tools for Real Estate Development',
+    },
+    {
+      hid: 'og:description',
+      property: 'og:description',
+      content: `Zenerate provides AI-powered feasibility solutions for real estate development. Explore all possibilities, maximize returns. Zenerate™ App.`,
+    },
+    {
+      hid: 'og:type',
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      hid: 'og:image:url',
+      property: 'og:image:url',
+      content: 'https://www.zenerate.ai/img/logo_og.png',
+    },
+    {
+      hid: 'og:image:type',
+      property: 'og:image:type',
       content: 'image/png',
     },
   ],
