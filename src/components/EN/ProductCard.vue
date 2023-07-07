78956f5ea0441747da5544adff87b05a4068976b
ckim-dev <ckim@zenerate.ai>
2023-07-07 16:35:01 +0900

    [QAQC] change ai consulting card made for pill background color

--- src/components/EN/ProductCard.vue
+++ src/components/EN/ProductCard.vue
@@ -108,8 +108,10 @@
         >
           <span
             v-for="userKey in CARD_DATA[product].bestFor"
-            class="text-13-medium mb-6 mr-6 h-29 w-fit rounded-60 px-12 leading-[29px] md:text-11-medium md:h-27 md:leading-[27px] lg:text-14-medium lg:mb-10 lg:h-36 lg:leading-[36px]"
-            :class="product === 'ai-consulting' ? 'bg-white' : 'bg-coolgray-50'"
+            class="text-13-medium mb-6 mr-6 h-29 w-fit rounded-60 bg-coolgray-50 px-12 leading-[29px] md:text-11-medium md:h-27 md:leading-[27px] lg:text-14-medium lg:mb-10 lg:h-36 lg:leading-[36px]"
+            :class="
+              product === 'ai-consulting' ? 'md:bg-white lg:bg-white' : ''
+            "
           >
             <span class="md:hidden lg:hidden"
               >{{ ROLES[userKey].icon }}&nbsp;&nbsp;{{
