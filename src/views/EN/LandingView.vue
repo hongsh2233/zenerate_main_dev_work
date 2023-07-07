a69d0b4be33b7ead266dd8de0d35c5652b0132ed
ckim-dev <ckim@zenerate.ai>
2023-07-07 16:35:29 +0900

    [QAQC] change text size of textimonial name

--- src/views/EN/LandingView.vue
+++ src/views/EN/LandingView.vue
@@ -86,8 +86,8 @@
                 :alt="`${data.logoText}`"
                 class="logo-img"
               />
-              <div class="ml-14 text-14 text-gray-700 md:text-18 lg:text-18">
-                <span class="font-medium text-black md:text-16 lg:text-16"
+              <div class="ml-14 text-14 text-gray-700 md:text-16 lg:text-16">
+                <span class="font-medium text-black md:text-18 lg:text-18"
                   >{{ data.name }},</span
                 ><br />{{ data.position }}
               </div>
