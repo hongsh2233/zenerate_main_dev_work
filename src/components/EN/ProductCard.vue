28d1a70b6d91e8952a952b03aca35e7e6e2007dc
ckim-dev <ckim@zenerate.ai>
2023-07-11 18:39:51 +0900

    Fix: remove unnecessary left padding on desktop view

--- src/components/EN/ProductCard.vue
+++ src/components/EN/ProductCard.vue
@@ -87,7 +87,7 @@
     </div>
 
     <div class="inner-shadow mt-auto h-fit py-20 px-22 md:px-60 lg:pl-84">
-      <div class="mb-24 pl-8 md:mb-14 md:pl-0">
+      <div class="mb-24 pl-8 md:mb-14 md:pl-0 lg:pl-0">
         <span
           class="mb-10 text-14 font-semibold text-primary md:hidden lg:hidden"
           >Best for</span
